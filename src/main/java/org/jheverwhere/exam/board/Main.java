95459c9651507afd40fb2ae98313b825c035f752
jheverywhere <nighteolt@naver.com>
2023-12-17 00:37:17 +0900

    시작 알림 메시지 출력

--- src/main/java/org/jheverwhere/exam/board/Main.java
+++ src/main/java/org/jheverwhere/exam/board/Main.java
@@ -2,6 +2,10 @@ package org.jheverwhere.exam.board;
 
 public class Main {
   public static void main(String[] args) {
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 게시판 시작==");
+    System.out.println("명령)");
+    System.out.println("== 게시판 실행 끝 ==");
 
   }
 }
