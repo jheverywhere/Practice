9406a9491ca617c62834fc6d1c9565c26991d55e
jheverywhere <nighteolt@naver.com>
2023-12-17 01:55:15 +0900

    입력받은 게시물 정보를 Article 객체에 저장 후 객체변수 출력

--- src/main/java/org/jheverwhere/exam/board/Main.java
+++ src/main/java/org/jheverwhere/exam/board/Main.java
@@ -16,10 +16,16 @@ public class Main {
       if (cmd.equals("/usr/article/write")) {
         System.out.println("==게시물 등록==");
         System.out.printf("제목 : ");
-        String cmd2 = in.nextLine();
+        String title = in.nextLine();
         System.out.printf("내용 : ");
-        String cmd3 = in.nextLine();
-        System.out.printf("%d번 게시물이 등록되었습니다. \n", id);
+        String body = in.nextLine();
+
+        Article article = new Article();
+        article.id = id;
+        article.title = title;
+        article.body = body;
+        System.out.println("생성 된 게시물 객체"+article);
+        System.out.printf("%d번 게시물이 등록되었습니다. \n", article.id);
         id++;
       } else if (cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
@@ -34,4 +40,11 @@ public class Main {
     in.close();
 
   }
+}
+class Article{
+  int id;
+  String title;
+  String body;
+
+
 }
