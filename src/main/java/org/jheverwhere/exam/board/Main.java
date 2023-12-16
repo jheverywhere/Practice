29e96fed5aa94dcc1f9fe135e085db8ed56b1480
jheverywhere <nighteolt@naver.com>
2023-12-17 01:07:47 +0900

    exit를 입력받기 전까지 명령어 계속입력

--- src/main/java/org/jheverwhere/exam/board/Main.java
+++ src/main/java/org/jheverwhere/exam/board/Main.java
@@ -8,9 +8,14 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 게시판 시작==");
-    System.out.printf("명령)");
-    String cmd = in.nextLine();
-    System.out.printf("입력된 명령어 : %s\n",cmd);
+    while(true){
+      System.out.printf("명령)");
+      String cmd = in.nextLine();
+      if(cmd.equals("exit")){
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }else System.out.printf("입력된 명령어 : %s\n",cmd);
+    }
     System.out.println("== 게시판 실행 끝 ==");
 
     in.close();
