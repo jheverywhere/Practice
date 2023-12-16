c01caa590a8edb69919aab00d851ad5d35390c5a
jheverywhere <nighteolt@naver.com>
2023-12-17 01:35:46 +0900

    C:/Program Files/Git/usr/article/write에 대한 명령어 처리

--- src/main/java/org/jheverwhere/exam/board/Main.java
+++ src/main/java/org/jheverwhere/exam/board/Main.java
@@ -8,16 +8,30 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 게시판 시작==");
-    while(true){
+
+    int id = 1;
+    while (true) {
       System.out.printf("명령)");
       String cmd = in.nextLine();
-      if(cmd.equals("exit")){
+      if (cmd.equals("/usr/article/write")) {
+        System.out.println("==게시물 등록==");
+        System.out.printf("제목 : ");
+        String cmd2 = in.nextLine();
+        System.out.printf("내용 : ");
+        String cmd3 = in.nextLine();
+        System.out.printf("%d번 게시물이 등록되었습니다. \n", id);
+        id++;
+      } else if (cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break;
-      }else System.out.printf("입력된 명령어 : %s\n",cmd);
-    }
-    System.out.println("== 게시판 실행 끝 ==");
+      } else {
+        System.out.println("잘못된 경로입니다. 다시 입력해주세요.");
+        continue;
+      }
 
+
+    }
     in.close();
+
   }
 }
